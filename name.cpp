338215e2bd744c65164ca126edaae0668f22c8da
Your Name <s3979760@rmit.edu.vn>
2024-12-19 14:08:26 +0700

    Update name.cpp

--- name.cpp
+++ name.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 using std::string;
 using namespace std;
+#include <ctime>
 class user
 {
 private:
    string username,password,fullname,email,idNumber;
-   int phoneNumber,rating;
+   int phoneNumber;
 public:
-    user(string username="",string password="",string fullname="",string email="",string idNumber="",int phoneNumber=0,int rating=0):username(username),fullname(fullname),email(email),idNumber(idNumber),phoneNumber(phoneNumber),rating(rating){}
+    user(string username="",string password="",string fullname="",string email="",string idNumber="",int phoneNumber=0):username(username),fullname(fullname),email(email),idNumber(idNumber),phoneNumber(phoneNumber){}
     ~user();
 };
+
+class member
+{
+private:
+    int creditPoint,rating;
+
+public:
+    member(int creditPoint=0,int rating=0):creditPoint(creditPoint),rating(rating){}
+    member();
+};
+class item
+{
+private:
+   string itemName,category,description,highestBidder;
+   int startingBid, currentBid,minBuyerRating;
+   time_t startDate,endDate;
+
+public:
+    item(string itemName=""){}
+    ~item();
+};
+
+item::item(/* args */)
+{
+}
+
+item::~item()
+{
+}
+
+
 
 int main(){
 }
