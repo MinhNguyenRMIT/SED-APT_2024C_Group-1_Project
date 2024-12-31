a05f09f7c556fe61c3257f2b3150a82d38ef3e32
s397976 <s3979760@rmit.edu.vn>
2025-01-01 00:16:14 +0700

    new

--- item.cpp
+++ item.cpp
@@ -9,6 +9,7 @@ private:
     string itemName,category ,description, highestBidder,ID;
     int startingBid,currentBid,minBuyerRating;
     time_t endTime,startTime,currentTime;
+    time(&currentTime);
 
 public:
     item(string ID,string itemName="",string category="",string description="",string highestBidder="",int startingBid=0,int currentBid=0,int minBuyerRating=0,time_t endTime=0,time_t startTime=0):ID(ID),itemName(itemName),category(category),description(description),highestBidder(highestBidder),startingBid(startingBid),currentBid(currentBid), minBuyerRating(minBuyerRating),endTime(endTime),startTime(startTime){
@@ -36,24 +37,45 @@ public:
     time_t getEndTime()  { return endTime; }
     time_t getStartTime()  { return startTime; }
 
-    void updateListing(){
+    void updateListing(item i){
+        ofstream file;
+        file.open("item.txt",ios::app);
+        if (file.is_open())
+        {
+            file<<i.ID<<" "<<i.itemName<<" "<<i.category<<" "<<i.description<<" "<<i.highestBidder<<" "<<i.startingBid<<" "<<i.currentBid<<" "<<i.minBuyerRating<<" "<<i.endTime<<" "<<i.startTime<<endl;
+            file.close();
+        }
+        
 
     };
-void addBid(string bidderName, int bidAmount,int rating){
-    if(bidAmount>currentBid && rating>=minBuyerRating){
-        currentBid = bidAmount;
-        highestBidder = bidderName;
+void addBid(string bidderName, int bidAmount,int rating,string itemID){
+    
+    if (itemID==ID && localtime(&currentTime) < localtime(&endTime)){
+        if(bidAmount>currentBid && rating>=minBuyerRating){
+            currentBid = bidAmount;
+            highestBidder = bidderName;
+        }
+        else if(bidAmount>currentBid && rating<minBuyerRating){
+            cout<<"You do not meet the minimum buyer rating requirement"<<endl;
+        }
+        else if(bidAmount<=currentBid){
+            cout<<"Your bid is lower than the current bid"<<endl;
     }
-    else if(bidAmount>currentBid && rating<minBuyerRating){
-        cout<<"You do not meet the minimum buyer rating requirement"<<endl;
     }
-    else if(bidAmount<=currentBid){
-        cout<<"Your bid is lower than the current bid"<<endl;
+    else if (itemID!=ID){
+        cout<<"Item ID does not match"<<endl;
     }
-
+    else if (localtime(&currentTime) >= localtime(&endTime)){
+        cout<<"Auction has ended"<<endl;
+    }
 
 };
-void concludeAuction(){
+void concludeAuction(item i){
+    if (localtime(&currentTime) == localtime(&i.endTime))
+    {
+        cout<<"Auction has ended"<<endl;
+    }
+    
     
 };
 };
@@ -69,6 +91,8 @@ int main()
     item i1("1","item1","category1","description1","bidder1",100,100,4,4000,0);
     time_t endTime = i1.getEndTime();
     cout << ctime(&endTime) << endl;
+    updateListing(i1);
     return 0;
+
 }
 
