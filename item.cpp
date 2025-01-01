edc1e6cc9c7afcabdde643f807cfc1de34e2dc5d
s397976 <s3979760@rmit.edu.vn>
2025-01-01 17:30:28 +0700

    basic update for item

--- item.cpp
+++ item.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
 using std::string;
 using namespace std;
-#include <ctime>
 
 class item
 {
 private:
     string itemName,category ,description, highestBidder,ID;
     int startingBid,currentBid,minBuyerRating;
-    time_t endTime,startTime,currentTime;
-    time(&currentTime);
+    time_t endTime, startTime, current_time;
+    
 
 public:
     item(string ID,string itemName="",string category="",string description="",string highestBidder="",int startingBid=0,int currentBid=0,int minBuyerRating=0,time_t endTime=0,time_t startTime=0):ID(ID),itemName(itemName),category(category),description(description),highestBidder(highestBidder),startingBid(startingBid),currentBid(currentBid), minBuyerRating(minBuyerRating),endTime(endTime),startTime(startTime){
-        
-    }
+     time(&current_time);   }
+
+
+
+
     void setID(string ID) { this->ID = ID; }
     void setItemName(string itemName) { this->itemName = itemName; }
     void setCategory(string category) { this->category = category; }
@@ -37,20 +41,35 @@ public:
     time_t getEndTime()  { return endTime; }
     time_t getStartTime()  { return startTime; }
 
-    void updateListing(item i){
-        ofstream file;
-        file.open("item.txt",ios::app);
-        if (file.is_open())
-        {
-            file<<i.ID<<" "<<i.itemName<<" "<<i.category<<" "<<i.description<<" "<<i.highestBidder<<" "<<i.startingBid<<" "<<i.currentBid<<" "<<i.minBuyerRating<<" "<<i.endTime<<" "<<i.startTime<<endl;
-            file.close();
-        }
-        
-
-    };
-void addBid(string bidderName, int bidAmount,int rating,string itemID){
     
-    if (itemID==ID && localtime(&currentTime) < localtime(&endTime)){
+
+    void updateListing(item i) {
+        ifstream infile("item.txt");
+        ofstream outfile("item_temp.txt", ios::app);
+        bool itemExists = false;
+        if (infile.is_open() && outfile.is_open()) {
+            string line;
+            while (getline(infile, line)) {
+                if (line.find(i.ID) != string::npos) {
+                    cout << "Item with ID " << i.ID << " already exists." << endl;
+                    itemExists = true;
+                }
+                outfile << line << endl;
+            }
+            if (!itemExists) {
+                outfile << i.ID << "," << i.itemName << "," << i.category << "," << i.description << "," << i.highestBidder << "," << i.startingBid << "," << i.currentBid << "," << i.minBuyerRating << "," << i.endTime << "," << i.startTime << endl;
+            }
+            infile.close();
+            outfile.close();
+            remove("item.txt");
+            rename("item_temp.txt", "item.txt");
+        } else {
+            cout << "Error opening file" << endl;
+        }
+    }
+    void addBid(string bidderName, int bidAmount,int rating,string itemID){
+    
+    if (itemID==ID && localtime(&current_time) < localtime(&endTime)){
         if(bidAmount>currentBid && rating>=minBuyerRating){
             currentBid = bidAmount;
             highestBidder = bidderName;
@@ -65,13 +84,13 @@ void addBid(string bidderName, int bidAmount,int rating,string itemID){
     else if (itemID!=ID){
         cout<<"Item ID does not match"<<endl;
     }
-    else if (localtime(&currentTime) >= localtime(&endTime)){
+    else if (localtime(&current_time) >= localtime(&endTime)){
         cout<<"Auction has ended"<<endl;
     }
 
 };
 void concludeAuction(item i){
-    if (localtime(&currentTime) == localtime(&i.endTime))
+    if (localtime(&current_time) == localtime(&i.endTime))
     {
         cout<<"Auction has ended"<<endl;
     }
@@ -89,9 +108,12 @@ void concludeAuction(item i){
 int main()
 {
     item i1("1","item1","category1","description1","bidder1",100,100,4,4000,0);
+    item i2("2","item2","category2","description2","bidder2",200,200,3,5000,0);
+    item i3("3","item3","category3","description3","bidder3",300,300,2,6000,0);     
+    i1.updateListing(i2);
     time_t endTime = i1.getEndTime();
-    cout << ctime(&endTime) << endl;
-    updateListing(i1);
+    
+    i1.updateListing(i1);
     return 0;
 
 }
